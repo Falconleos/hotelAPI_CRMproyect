66308ccef5946b4ff9fdb31f0a4701d6c382dc23
Falconleos <leonelfsotopublic@gmail.com>
2026-07-17 13:31:54 -0300

    feature:with credential true

--- src/main/java/com/example/hotelAPI/controller/AuthController.java
+++ src/main/java/com/example/hotelAPI/controller/AuthController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/public/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @Tag(name = "Autenticación", description = "Endpoints públicos para la gestión de accesos, registro y tokens")
 public class AuthController {
 
