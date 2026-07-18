9c9b9d7b68a6da0f174cf01a6bba4c67e4a0050c
Leonel Federico Soto <leonelfsotopublic@gmail.com>
2026-07-18 18:44:11 -0300

    feature: permiso para ser onsumida por el puerto 4200 de angualar

--- src/main/java/com/example/hotelAPI/controller/EmployeeController.java
+++ src/main/java/com/example/hotelAPI/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/private/employee")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @Tag(name = "GestionEmpleados", description = "Endpoints privados para la administración del personal del hotel")
 public class EmployeeController {
 
